7eb585db91eb72d981b6da31e00e366d19705fbc
Kacper Stokwisz <kacperstokwisz@gmail.com>
2020-07-15 22:49:11 +0200

    scramble list items

--- script.js
+++ script.js
@@ -22,6 +22,9 @@ let dragStartIndex;
 // Insert list items into DOM
 function createList() {
     [...richestPeople]
+      .map(a => ({ value: a, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(a => a.value)
       .forEach((person, index) => {
         const listItem = document.createElement('li');
   
